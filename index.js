239a8b558e38b70d25c252e6697e8e36fcfa15b5
Luca Nollet <luca.nollet@student.pxl.be>
2026-01-09 15:09:13 +0100

    [05] Stijl helemaal aangepast

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
+const projectenContainer = document.querySelector('.projecten');
+const projecten = document.querySelectorAll('.project');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
 
-    const projectenContainer = document.querySelector('.projecten');
-    const projecten = document.querySelectorAll('.project');
-    const prevBtn = document.getElementById('prev');
-    const nextBtn = document.getElementById('next');
+let currentIndex = 0;
 
-    let currentIndex = 0;
-
-    function updateSlider() {
+function updateSlider() {
     projectenContainer.style.transform = `translateX(-${currentIndex * 100}%)`;
 }
 
-    prevBtn.addEventListener('click', () => {
+prevBtn.addEventListener('click', () => {
     currentIndex--;
-    if (currentIndex < 0) currentIndex = projecten.length - 1;
+    if (currentIndex < 0) currentIndx = projecten.length - 1;
     updateSlider();
 });
 
-    nextBtn.addEventListener('click', () => {
+nextBtn.addEventListener('click', () => {
     currentIndex++;
     if (currentIndex >= projecten.length) currentIndex = 0;
     updateSlider();
 });
-
 
 
